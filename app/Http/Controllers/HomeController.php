c088d0068e1724cdf19939109998cb89286c0c53
Jacopo <jakonigro@gmail.com>
2020-10-26 17:21:43 +0100

    problema con creazione tabelle

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+  public function index ()
+  {
+    $data = [
+            'students' => ['Michele', 'Fabio'],
+            'teachers' => ['Roberto', 'Fabio']
+            ];
+    return view("index", compact("data"));
+  }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>ciao</h1>
+    @dd ($data)
+  </body>
+</html>
